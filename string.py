622f0b0471efaa88e8fc413c5c7a8b1c8fcc63b6
Amazing Cabiles <clappalc12@gmail.com>
2021-10-22 01:48:20 +0800

    updated

--- string.py
+++ string.py
@@ -1,3 +1,6 @@
+# Note: this is just some sample code demonstrating that you can use <= signs
+# or what not in python and the program will still work. 
+
 if __name__ == "__main__":
     name = "Oh, Sehun"
     age = "27"
@@ -5,11 +8,12 @@ if __name__ == "__main__":
     bp = "120/80"
 
     if temp <= "37" and bp <= "120/80":
-        print("Person is now vaccinated")
+        result = name + " " + age + " " + temp + " " + bp
+        print(result)
     elif temp <= "37" and bp <= "120/80":
         print("Person's temperature and bp will get rechecked")
     else:
         print("Sorry, you won't get vaccinated. Please go home.”")
 
-    result = name + " " + age + " " + temp + " " + bp
-    print(result)
+# Sample Output
+# Oh, Sehun 27 36.1 120/80
